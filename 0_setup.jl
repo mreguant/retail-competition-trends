cd0f376eac4808e4510ef4d07784a825f0fe9795
Ruoyililin <ruoyililin@gmail.com>
2022-03-08 10:32:38 +0100

    Update 0_setup.jl

--- 0_setup.jl
+++ 0_setup.jl
@@ -18,20 +18,24 @@ end
 
 required_packages = [
     "CSV", "DataFrames", "StringEncodings", "ShiftedArrays","Statistics",
-    "JuMP", "Ipopt","DataStructures", "Printf",
+    "Ipopt","DataStructures", "Printf",
     "FixedEffectModels", "CategoricalArrays", "RegressionTables","Plots","Dates"
 ]
 
 ### check which packages are already installed
 
 # deprecated, but fastest version to check installed packages
-using Pkg
+import Pkg
 installed_packages = keys(Pkg.installed())
-
 # not deprecated, but slowest version
 # installed_packages = [collect(values(Pkg.dependencies()))[p].name for p in 1:length(Pkg.dependencies())]
 
 install_packages(required_packages, installed_packages)
+
+
+# Install lastest version of JuMP so that it precompiles properly with Julia 1.6+
+Pkg.add(Pkg.PackageSpec(;name="JuMP", version="0.21.10"))
+
 
 println("\n
 The list of required packages has been successfully installed.")
